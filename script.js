b151e8ac893f86aa99eda8967f50b72076c3faa2
alekcandr1 <alekcandrmain@gmail.com>
2023-10-03 15:48:02 +0300

    math

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ var = a;
 a = 7;
 var = b;
 b = a;
+
+var myFirstName = "Alex";
+var myLastName = "Bohon";
+
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
