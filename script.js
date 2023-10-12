c045e44d750e2eb81edb912ad106749f9286464a
alekcandr1 <alekcandrmain@gmail.com>
2023-10-12 13:09:38 +0300

    Manipulate Arrays With push Method

--- script.js
+++ script.js
@@ -175,7 +175,27 @@ const myArray = [[1, 2], [3, 5]];
 
 //4 Access Array Data with Indexes
 const myArray = [50, 60, 70];
+const myData = myArray[0];
+console.log(myData);
 
+//5 Modify Array Data With Indexes
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45;
 
+// Only change code below this line
 
+//6 Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+  console.log(myData);
 
+//7 Manipulate Arrays With push Method
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
